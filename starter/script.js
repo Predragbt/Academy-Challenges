7bacb2cb9d098a319d8aee9734755aa63df6654d
Predrag Pavlovikj <predragnbt@gmail.com>
2024-04-16 22:18:07 +0200

    Finished 3rd exercise.

--- starter/script.js
+++ starter/script.js
@@ -16,10 +16,23 @@ function displayImg() {
 
   btnNext.disabled = currentImageIndex === images.length - 1;
   btnPrevious.disabled = currentImageIndex === 0;
+
+  if (currentImageIndex === images.length - 1) {
+    document.removeEventListener("keyup", keyUpImgNext);
+  } else {
+    document.addEventListener("keyup", keyUpImgNext);
+  }
+
+  if (currentImageIndex === 0) {
+    document.removeEventListener("keyup", keyUpImgPrevous);
+  } else {
+    document.addEventListener("keyup", keyUpImgPrevous);
+  }
 }
 
 function imgNext() {
   currentImageIndex++;
+
   displayImg();
 }
 
@@ -28,7 +41,22 @@ function imgPrevous() {
   displayImg();
 }
 
+function keyUpImgNext(event) {
+  if (event.key === "ArrowRight") {
+    imgNext();
+  }
+}
+
+function keyUpImgPrevous(event) {
+  if (event.key === "ArrowLeft") {
+    imgPrevous();
+  }
+}
+
 btnNext.addEventListener("click", imgNext);
 btnPrevious.addEventListener("click", imgPrevous);
+
+document.addEventListener("keyup", keyUpImgNext);
+document.addEventListener("keyup", keyUpImgPrevous);
 
 displayImg();
