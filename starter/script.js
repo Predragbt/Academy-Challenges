5f6f7ed194c524fb589f7129c1df9cf5afcda14a
Predrag Pavlovikj <predragnbt@gmail.com>
2024-05-21 00:04:06 +0200

    Added starter files.

---
+++ starter/script.js
@@ -0,0 +1,46 @@
+// Write the JavaScript class Weather here
+class Weather {
+  constructor(city, temperature, humidity, windSpeed) {}
+
+  // Exercise 01
+  static fetchWeather(city) {}
+
+  static displayCurrentWeather(data) {}
+
+  // Exercise 02
+  static fetchForecast(city) {}
+
+  static displayForecast(data) {}
+}
+
+//Uncomment the lines below to use weather database
+// Weather.weatherData = [
+//   new Weather('New York', 16, 70, 7),
+//   new Weather('London', 12, 80, 5),
+//   new Weather('Tokyo', 22, 60, 4),
+//   new Weather('Sydney', 25, 50, 6),
+//   new Weather('Paris', 15, 65, 5),
+//   new Weather('Berlin', 14, 60, 6),
+//   new Weather('Moscow', 5, 75, 10),
+//   new Weather('Toronto', 17, 55, 8),
+//   new Weather('Rio de Janeiro', 26, 85, 7),
+//   new Weather('Beijing', 20, 40, 3),
+//   new Weather('Mumbai', 30, 70, 5),
+//   new Weather('Los Angeles', 19, 65, 4),
+//   new Weather('Cape Town', 18, 60, 6),
+//   new Weather('Rome', 21, 55, 3),
+//   new Weather('Bangkok', 33, 75, 2),
+//   new Weather('Istanbul', 20, 60, 4),
+//   new Weather('Lagos', 29, 80, 3),
+//   new Weather('Buenos Aires', 23, 70, 5),
+//   new Weather('Chicago', 10, 65, 7),
+//   new Weather('Shanghai', 19, 80, 6),
+// ];
+
+//Exercise 01
+function searchWeather() {}
+
+// Exercise 03
+function saveRecentSearch(city) {}
+
+function displayRecentSearches() {}
