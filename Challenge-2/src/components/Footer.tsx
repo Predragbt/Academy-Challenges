d17d3fc96841b52d315477504fa1e0a7347ac53d
Predrag Pavlovikj <predragnbt@gmail.com>
2024-08-26 23:21:10 +0200

    Started Challenge 2

---
+++ Challenge-2/src/components/Footer.tsx
@@ -0,0 +1,226 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+export const Footer = () => {
+  return (
+    <footer className="footer d-flex justify-content-between p-3">
+      <div className="footer-section flex-1">
+        <h6 className="fw-bold">Social share</h6>
+        <div className="footer-icons d-flex align-items-center">
+          <div className="footer-icon-box d-flex justify-content-center align-items-center me-1 p-2  ">
+            <FontAwesomeIcon
+              icon={faFacebookF}
+              className="footer-icon text-white"
+            />
+          </div>
+          <div className="footer-icon-box d-flex justify-content-center align-items-center me-1 p-2">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="footer-icon text-white"
+            />
+          </div>
+          <div className="footer-icon-box d-flex justify-content-center align-items-center me-1 p-2">
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className="footer-icon text-white"
+            />
+          </div>
+          <div className="footer-icon-box d-flex justify-content-center align-items-center me-1 p-2">
+            <FontAwesomeIcon
+              icon={faLinkedinIn}
+              className="footer-icon text-white"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="footer-section">
+        <h6 className="fw-bold">Event info</h6>
+        <ul className="list-unstyled m-0">
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Enter Now
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Event Info
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Course Maps
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Race Pack
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Results
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              FAQs
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Am I Registered?
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h6 className="fw-bold">Registration</h6>
+        <ul className="list-unstyled m-0">
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Volunteers
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Gallery
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Press
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Results
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Privacy Policy
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Service Plus
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Contacts
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h6 className="fw-bold">Schedule</h6>
+        <ul className="list-unstyled m-0">
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Gallery
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              About
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Videos
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Results
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              FAQs
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Results
+            </a>
+          </li>
+          <li className="m-0">
+            <a
+              href="#"
+              className="text-decoration-none text-dark footer-hover-effect"
+            >
+              Volunteers
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
---
+++ Challenge-2/src/components/Header.tsx
@@ -0,0 +1,48 @@
+export const Header = () => {
+  return (
+    <header className="w-100 py-2">
+      <ul className="d-flex list-unstyled justify-content-center m-0">
+        <li className="text-uppercase me-2">
+          <a href="#home" className="text-decoration-none text-dark">
+            Home
+          </a>
+        </li>
+        <li className="text-uppercase me-2">
+          <a href="#bikes" className="text-decoration-none text-dark">
+            Bikes
+          </a>
+        </li>
+        <li className="text-uppercase me-2">
+          <a href="#gear" className="text-decoration-none text-dark">
+            Gear
+          </a>
+        </li>
+        <li className="text-uppercase me-2">
+          <a href="#parts" className="text-decoration-none text-dark">
+            Parts
+          </a>
+        </li>
+        <li className="text-uppercase me-2">
+          <a href="#tires" className="text-decoration-none text-dark">
+            Tires
+          </a>
+        </li>
+        <li className="text-uppercase me-2">
+          <a href="#service-info" className="text-decoration-none text-dark">
+            Service Info
+          </a>
+        </li>
+        <li className="text-uppercase me-2">
+          <a href="#catalogues" className="text-decoration-none text-dark">
+            Catalogues
+          </a>
+        </li>
+        <li className="text-uppercase me-2">
+          <a href="#contact" className="text-decoration-none text-dark">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+};
